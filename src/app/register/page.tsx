1602f97fe8632bc1a19b5584d0c199fb3be64bbb
shaswatk45 <147980873+shaswatk45@users.noreply.github.com>
2025-03-16 13:05:41 +0530

    Register page (#12)
    * Added /register route and updated login page link

---
+++ src/app/register/page.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <h1 className="text-3xl font-bold text-center">!REGISTRATION PAGE COMING SOON!</h1>
+        </div>
+    );
+}
